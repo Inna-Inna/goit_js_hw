60cb6f4fb06a32727abe5de5886f77b355d446e3
Inna <zhurbainna@gmail.com>
2015-09-24 03:02:03 +0300

    lesson3 task 5

--- lesson3/task5.js
+++ lesson3/task5.js
@@ -1,16 +1,16 @@
 var getGrid = function(gridSize){
-    var tile = "\n";
-    for (var i = 1; i < gridSize; i++){
-        for(var j = 1; j <= gridSize; j++){
-            if ((j+i)%2 ===0 ){
-                tile += "#";
+    var tiles = "\n";
+    for (var i = 0; i < gridSize; i++){
+        for(var j = 0; j < gridSize; j++){
+            if ((j+i)%2 === 0 ){
+                tiles += " ";
             } else {
-                tile += " ";
+                tiles += "#";
             }
         }
-        tile+="\n";
+        tiles+="\n";
     }
-    return tile;
+    return tiles;
 };
 
 console.log(getGrid(8));

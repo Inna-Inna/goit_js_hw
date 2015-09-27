f5b39da968e12708cef094c8cf9ff5c7574b3e06
Inna-Inna <zhurbainna@gmail.com>
2015-09-28 02:09:01 +0300

    lesson 4, task 2

--- lesson4/task2.js
+++ lesson4/task2.js
@@ -1,14 +1,4 @@
-function checkSpam(msg){
-    msg = msg.toString();
-    msg = msg.toLocaleLowerCase();
-    var count = 0;
-    var bannedWord1 = "sex";
-    var bannedWord2 = "spam";
-    while(true){
-        var indexOfBanned = msg.indexOf(bannedWord1, count);
-        if (count == -1) break;
-        alert("Spammer!!!");
-        count = indexOfBanned+1;
-    }
-    return msg;
+function checkSpam(msg) {
+    msg = msg.toLowerCase();
+    return !!(msg.indexOf("sex") != -1 || msg.indexOf("spam") != -1);
 }
